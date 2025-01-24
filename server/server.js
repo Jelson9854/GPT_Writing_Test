19f29059e27274ce7546aedae012446bef7666ad
Jelson9854 <Jelson9854@vt.edu>
2025-01-24 14:53:46 -0500

    CORS change

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 const corsOptions = {
- 	origin: 'https//gptwriting.cs.vt.edu',
+ 	origin: 'https://gptwriting.cs.vt.edu',
  	methods: '[POST]',
  	credentials: true
  };
